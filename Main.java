42d1828c6867abe9e2e88cb60d035ac058e25a0c
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-10-29 23:11:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,77 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        //Q2
+        //int[] numbers = {1, 4, 17, 7, 25, 3, 100};
+        //int sum = 0;
+        //for (int i = 0; i < numbers.length; i++) {
+        //   sum = sum + numbers[i];
+        //  System.out.println(sum);
+
+        //}
+        //for (int n : numbers) {
+        //   int average = sum / 7;
+        //   System.out.println(average);
+        //   if (numbers[n] > average) {
+        //      System.out.println(numbers[n]);
+        //}
+
+
+        //Q3
+        //int[] numbers = {20, 30, 40};
+        //int large = numbers[0];
+        //if (numbers[2] > large) {
+        //    large = numbers[2];
+
+
+        //}
+        //System.out.println("the larger value is:" + large);
+
+        //Q1
+        //int [] numbers = { 50 , -20 , 0, 30, 40, 60 ,10};
+        //if (numbers[0]==numbers.length-1){
+        //   System.out.println("Equal");
+        //} else System.out.println("not equal");
+
+        //Q4
+        //int[] numbers = {20, 30, 40};
+        //int one = numbers[0];
+        //int i = 0;
+        //numbers[0] = numbers[numbers.length - 1];
+        //numbers[numbers.length - 1] = one;
+        //for (i = 0; i < numbers.length; i++) {
+        //    System.out.println(numbers[i]);
+
+        //Q5
+        //  int [] numbers = { 2,3,40,1,5,9,4,10,7};
+        // for(int n: numbers){
+        //  if (n%1==0)
+        //     System.out.println("odd" + n);
+        //  }
+        //  for (int m: numbers){
+        //   if (m%2==0)
+        //      System.out.println("even" + m);
+        //}
+
+        //Q6
+        //int [] numbers1 ={2,3,6,6,4};
+        //int [] numbers2 ={2,3,6,6,4};
+        //int a = numbers1.length;
+        //int b = numbers2.length;
+        //if (a==b){
+        //   System.out.println("Equal");
+        //  } else System.out.println("not equal");
+
+    }
+}
+
+
+
+
+
+
+
+
+
